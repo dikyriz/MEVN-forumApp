dd34f8dd89a2cde11582eed6553a35bd43fda8a8
dikyriz <rizaldi.cool4@gmail.com>
2024-07-31 09:04:35 +0700

    part-27

---
+++ client/src/components/Question/FormQuestion.vue
@@ -0,0 +1,85 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <AlertMessage
+      v-if="errorAlert"
+      :message="errorMsg"
+      style="max-width: 100%"
+    />
+    <div class="flex align-items-center gap-3 mb-3">
+      <InputText
+        v-model="question.title"
+        class="flex-auto"
+        placeholder="Insert Question Title"
+      />
+    </div>
+    <div class="flex align-items-center gap-3 mb-5">
+      <Editor
+        editorStyle="height: 320px"
+        style="width: 100%"
+        placeholder="Insert Your Question"
+        v-model="question.question"
+      />
+    </div>
+    <div class="flex align-items-center gap-3 mb-5">
+      <Dropdown
+        v-model="question.category"
+        :options="categories"
+        placeholder="Select a Category Question"
+        class="w-full"
+      />
+    </div>
+    <div class="flex justify-content-end gap-2">
+      <Button
+        type="button"
+        label="Cancel"
+        severity="secondary"
+        @click="$emit('close')"
+      ></Button>
+      <Button type="submit" label="Submit"></Button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+import customFetch from "@/api";
+import AlertMessage from "../AlertMessage.vue";
+
+const emit = defineEmits(["close"]);
+
+//state alert
+const errorAlert = ref(false);
+const errorMsg = ref();
+
+const question = reactive({
+  title: "",
+  question: "",
+  category: "",
+});
+
+const clearInput = () => {
+  question.title = "";
+  question.question = "";
+  question.category = "";
+};
+
+const handleSubmit = async () => {
+  try {
+    const QuestionData = await customFetch.post("/question", {
+      title: question.title,
+      question: question.question,
+      category: question.category,
+    });
+
+    if (QuestionData) {
+      clearInput();
+      emit("close");
+    }
+  } catch (error) {
+    errorAlert.value = true;
+    errorMsg.value = error.response.data.message;
+  }
+};
+
+const categories = ref(["javascript", "database", "nodejs", "vuejs"]);
+</script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,3 +1,52 @@
 <template>
-  <h1>test home</h1>
+  <main>
+    <div class="grid">
+      <aside class="col-2">
+        <div class="text-center p-3 border-round-sm bg-blue-100 font-bold">
+          Filter
+        </div>
+      </aside>
+      <section class="col">
+        <div class="p-3 border-round-sm bg-blue-50">
+          <div class="flex justify-content-between align-items-center">
+            <h2 class="text-4xl text-primary">List Question</h2>
+            <Button
+              label="Tambah"
+              rounded
+              type="button"
+              @click="dialog = true"
+              icon="pi pi-plus"
+            />
+          </div>
+
+          <Dialog
+            v-model:visible="dialog"
+            modal
+            header="Create Question"
+            :style="{ width: '70%' }"
+          >
+            <FormQuestion @close="closeDialog()" />
+          </Dialog>
+
+          <ListQuestion />
+          <ListQuestion />
+          <ListQuestion />
+          <ListQuestion />
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import ListQuestion from "@/components/Question/ListQuestion.vue";
+import Dialog from "primevue/dialog";
+import FormQuestion from "@/components/Question/FormQuestion.vue";
+import { ref } from "vue";
+
+const dialog = ref(false);
+
+const closeDialog = () => {
+  dialog.value = false;
+};
+</script>
