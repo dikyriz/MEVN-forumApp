f3a6d36840f467ec77d77c38a0f9b00d65972f00
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2025-01-26 08:51:11 +0700

    update config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,26 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+  return defineConfig({
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-});
+  });
+}
